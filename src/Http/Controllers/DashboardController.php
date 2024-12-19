2edf9fdf6e783000f0ba089766fa77b794b1b414
gh-actions <gh@robertogallea.com>
2024-12-19 03:01:29 +0000

    Laravel Pint

--- src/Http/Controllers/DashboardController.php
+++ src/Http/Controllers/DashboardController.php
@@ -5,22 +5,25 @@ namespace Robertogallea\PulseApi\Http\Controllers;
 use Illuminate\Http\Request;
 use Robertogallea\PulseApi\Http\Resources\DashboardResource;
 
-class DashboardController {
-    public function index(Request $request) {
+class DashboardController
+{
+    public function index(Request $request)
+    {
         $period = $request->query('period', '');
 
         return new DashboardResource(null, $period);
     }
 
-    public function show(Request $request, string $type) {
+    public function show(Request $request, string $type)
+    {
         $period = $request->query('period', '');
         if (array_key_exists($type, config('pulse-api.resources')->toArray())) {
-            return new (config('pulse-api.resources.' . $type))(null, $period);
+            return new (config('pulse-api.resources.'.$type))(null, $period);
         }
 
         return response()->json([
             'data' => [
-                'message' => 'Metric type "' . $type . '" does not exist.',
+                'message' => 'Metric type "'.$type.'" does not exist.',
             ],
         ], 404);
     }
