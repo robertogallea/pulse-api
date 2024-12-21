a398ffcf2ec485037a0d345efef907ffaa314acc
gh-actions <gh@robertogallea.com>
2024-12-21 05:08:38 +0000

    Laravel Pint

--- src/Http/Resources/DashboardResource.php
+++ src/Http/Resources/DashboardResource.php
@@ -4,8 +4,10 @@ namespace Robertogallea\PulseApi\Http\Resources;
 
 use Illuminate\Http\Request;
 
-class DashboardResource extends PulseResource {
-    public function toArray(Request $request): array {
+class DashboardResource extends PulseResource
+{
+    public function toArray(Request $request): array
+    {
         $resources = config('pulse-api.resources');
 
         $result = $resources->mapWithKeys(function (string $resource, string $key) use ($request) {
