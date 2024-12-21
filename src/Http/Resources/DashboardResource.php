51392fb1651e2d72995e8f287552e6f6c36a8cb3
aminnausin <aminnausin@outlook.com>
2024-12-21 00:08:12 -0500

    Use period on dashboard resource builder

--- src/Http/Resources/DashboardResource.php
+++ src/Http/Resources/DashboardResource.php
@@ -4,14 +4,12 @@ namespace Robertogallea\PulseApi\Http\Resources;
 
 use Illuminate\Http\Request;
 
-class DashboardResource extends PulseResource
-{
-    public function toArray(Request $request): array
-    {
+class DashboardResource extends PulseResource {
+    public function toArray(Request $request): array {
         $resources = config('pulse-api.resources');
 
         $result = $resources->mapWithKeys(function (string $resource, string $key) use ($request) {
-            return (new $resource(null))->toArray($request);
+            return (new $resource(null, $this->period))->toArray($request);
         });
 
         return $result->toArray();
